093cddf87c05935d9be25aa8c94583be0738daff
SATHYA <sathya147@gmail.com>
2021-12-08 08:01:19 +0530

    testng and surefire plugin

--- src/test/java/testpackagaarunaagum/NeeOodu.java
+++ src/test/java/testpackagaarunaagum/NeeOodu.java
@@ -1,10 +1,17 @@
 package testpackagaarunaagum;
 
+import org.testng.annotations.Test;
+
 public class NeeOodu {
 	
 	public static void main(String[] args) {
 		
 		System.out.println("kandupudicha kandupudicha with some knowledge and logical thinking");
+	}
+	
+	@Test
+	public void tryingTestNG() {
+		System.out.println("Trying Jenkins and maven using TestNG");
 	}
 
 }
