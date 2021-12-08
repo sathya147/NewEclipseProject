b52fc39d3e6b377e6c64568da9b8a28d2058b36f
SATHYA <sathya147@gmail.com>
2021-12-08 07:40:50 +0530

    test la code oduma

---
+++ src/test/java/testpackagaarunaagum/NeeOodu.java
@@ -0,0 +1,10 @@
+package testpackagaarunaagum;
+
+public class NeeOodu {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("kandupudicha kandupudicha with some knowledge and logical thinking");
+	}
+
+}
